c4a6de99a942924e9cd5031c68addf23c152e497
Ngoan Ho <ngoanh2n@gmail.com>
2021-04-10 14:25:46 +0700

    [wdc][selenide] Add InstanceFromSelenide

---
+++ webdriverchecker-selenide/src/main/java/io/github/ngoanh2n/wdc/InstanceFromSelenide.java
@@ -0,0 +1,17 @@
+package io.github.ngoanh2n.wdc;
+
+import com.codeborne.selenide.WebDriverRunner;
+import org.openqa.selenium.WebDriver;
+
+/**
+ * @author Ho Huu Ngoan (ngoanh2n@gmail.com)
+ * @version 1.0.0
+ * @since 2021-04-10
+ */
+public class InstanceFromSelenide implements InstanceProvider {
+
+    @Override
+    public WebDriver provide() {
+        return WebDriverRunner.getWebDriver();
+    }
+}
