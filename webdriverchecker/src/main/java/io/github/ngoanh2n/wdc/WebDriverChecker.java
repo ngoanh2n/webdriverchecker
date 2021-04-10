a9b23a413df19dab91abff9345090922269fe23e
Ngoan Ho <ngoanh2n@gmail.com>
2021-04-10 15:22:13 +0700

    [wdc] Add InstanceChecker

--- webdriverchecker/src/main/java/io/github/ngoanh2n/wdc/WebDriverChecker.java
+++ webdriverchecker/src/main/java/io/github/ngoanh2n/wdc/WebDriverChecker.java
@@ -1,18 +1,9 @@
 package io.github.ngoanh2n.wdc;
 
-import org.openqa.selenium.Capabilities;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.remote.RemoteWebDriver;
-
 /**
  * @author Ho Huu Ngoan (ngoanh2n@gmail.com)
  * @version 1.0.0
  * @since 2021-04-10
  */
 public class WebDriverChecker {
-
-    private Capabilities getCapabilities() {
-        WebDriver driver = InstanceProviderLoader.load().provide();
-        return ((RemoteWebDriver) driver).getCapabilities();
-    }
 }
