827bbb2394398123db704e5ff141d95c26ac7dae
Ngoan Ho <ngoanh2n@gmail.com>
2021-04-10 15:18:15 +0700

    [wdc] Remove logger

--- webdriverchecker/src/main/java/io/github/ngoanh2n/wdc/InstanceProviderLoader.java
+++ webdriverchecker/src/main/java/io/github/ngoanh2n/wdc/InstanceProviderLoader.java
@@ -1,11 +1,7 @@
 package io.github.ngoanh2n.wdc;
 
-import org.slf4j.Logger;
-
 import java.util.Iterator;
 import java.util.ServiceLoader;
-
-import static org.slf4j.LoggerFactory.getLogger;
 
 /**
  * @author Ho Huu Ngoan (ngoanh2n@gmail.com)
@@ -14,18 +10,14 @@ import static org.slf4j.LoggerFactory.getLogger;
  */
 class InstanceProviderLoader {
 
-    private static final Logger LOGGER = getLogger(InstanceProviderLoader.class);
-
     static synchronized InstanceProvider load() {
         ServiceLoader<InstanceProvider> service = ServiceLoader.load(InstanceProvider.class);
         Iterator<InstanceProvider> serviceLoaders = service.iterator();
 
-        if (!serviceLoaders.hasNext()) {
-            throw new IllegalStateException("No implementation of InstanceProvider provided");
+        if (serviceLoaders.hasNext()) {
+            return serviceLoaders.next();
         } else {
-            InstanceProvider implementation = serviceLoaders.next();
-            LOGGER.info("Using implementation of {}: {}", InstanceProvider.class.getName(), implementation.getClass().getName());
-            return implementation;
+            throw new IllegalStateException("No implementation of InstanceProvider provided");
         }
     }
 }
