7a3ed2e6131a9374165b0dd79979051a9acf9b05
Ngoan Ho <ngoanh2n@gmail.com>
2021-04-10 15:31:10 +0700

    [wdc] Add SeleniumTest.isChromeTest()

--- webdriverchecker-selenide/src/test/java/io/github/ngoanh2n/wdc/SeleniumTest.java
+++ webdriverchecker-selenide/src/test/java/io/github/ngoanh2n/wdc/SeleniumTest.java
@@ -1,9 +1,31 @@
 package io.github.ngoanh2n.wdc;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.closeWebDriver;
+import static com.codeborne.selenide.Selenide.open;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 /**
  * @author Ho Huu Ngoan (ngoanh2n@gmail.com)
  * @version 1.0.0
  * @since 2021-04-10
  */
 public class SeleniumTest {
+
+    @Test
+    void isChromeTest() {
+        assertTrue(WebDriverChecker.isChrome());
+    }
+
+    @BeforeEach
+    void openDriver() {
+        open("https://github.com");
+    }
+
+    @BeforeEach
+    void closeDriver() {
+        closeWebDriver();
+    }
 }
