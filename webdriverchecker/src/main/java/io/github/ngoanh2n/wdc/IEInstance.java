d3e5339c813394eb4a2b200ec4ca293316b75222
Ngoan Ho <ngoanh2n@gmail.com>
2021-04-10 16:57:21 +0700

    [wdc] Add IEInstance

---
+++ webdriverchecker/src/main/java/io/github/ngoanh2n/wdc/IEInstance.java
@@ -0,0 +1,18 @@
+package io.github.ngoanh2n.wdc;
+
+import org.openqa.selenium.ie.InternetExplorerDriver;
+
+/**
+ * @author Ho Huu Ngoan (ngoanh2n@gmail.com)
+ * @version 1.0.0
+ * @since 2021-04-10
+ */
+class IEInstance extends InstanceChecker<Boolean> {
+
+    private static final String NAME = "internet explorer";
+
+    @Override
+    public Boolean check() {
+        return getBrowserName().equals(NAME) || getDriver() instanceof InternetExplorerDriver;
+    }
+}
