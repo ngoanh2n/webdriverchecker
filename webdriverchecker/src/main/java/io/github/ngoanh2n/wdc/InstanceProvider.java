9a03fc9cdd886b7fb8d84530ce6ce81929e932d5
Ngoan Ho <ngoanh2n@gmail.com>
2021-04-10 13:33:21 +0700

    [wdc] Add InstanceProvider

---
+++ webdriverchecker/src/main/java/io/github/ngoanh2n/wdc/InstanceProvider.java
@@ -0,0 +1,13 @@
+package io.github.ngoanh2n.wdc;
+
+import org.openqa.selenium.WebDriver;
+
+/**
+ * @author Ho Huu Ngoan (ngoanh2n@gmail.com)
+ * @version 1.0.0
+ * @since 2021-04-10
+ */
+public interface InstanceProvider {
+
+    WebDriver provide();
+}
