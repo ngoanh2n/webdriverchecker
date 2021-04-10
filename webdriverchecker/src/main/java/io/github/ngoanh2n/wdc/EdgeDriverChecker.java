7fe323d4ed58b36610068cfd56b4b9db13c3db10
Ngoan Ho <ngoanh2n@gmail.com>
2021-04-10 16:16:34 +0700

    [wdc] Add EdgeDriverChecker

---
+++ webdriverchecker/src/main/java/io/github/ngoanh2n/wdc/EdgeDriverChecker.java
@@ -0,0 +1,20 @@
+package io.github.ngoanh2n.wdc;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+
+/**
+ * @author Ho Huu Ngoan (ngoanh2n@gmail.com)
+ * @version 1.0.0
+ * @since 2021-04-10
+ */
+@SuppressWarnings("SpellCheckingInspection")
+class EdgeDriverChecker extends InstanceChecker<Boolean> {
+
+    private static final String NAME = "msedge";
+
+    @Override
+    public Boolean check() {
+        return getBrowserName().equals(NAME) || getDriver() instanceof EdgeDriver;
+    }
+}
