d3a0ac752f6cb6c7731d69b7e7838067e6e01a30
Ngoan Ho <ngoanh2n@gmail.com>
2022-01-23 23:32:42 +0700

    [wdc] Separate implementation of checkers

---
+++ webdriverchecker/src/main/java/io/github/ngoanh2n/wdc/WDCType.java
@@ -0,0 +1,34 @@
+package io.github.ngoanh2n.wdc;
+
+/**
+ * @author Ho Huu Ngoan (ngoanh2n@gmail.com)
+ * @version 1.0.0
+ * @since 2021-04-10
+ */
+@SuppressWarnings("SpellCheckingInspection")
+enum WDCType {
+
+    // Browsers
+    EDGE("msedge"),
+    OPERA("opera"),
+    SAFARI("safari"),
+    CHROME("chrome"),
+    FIREFOX("firefox"),
+    IE("internetexplorer"),
+    EDGE_LEGACY("microsoftedge"),
+
+    // Platforms
+    IOS("ios"),
+    ANDROID("android"),
+    WINDOWS("windows");
+
+    private final String name;
+
+    WDCType(String name) {
+        this.name = name;
+    }
+
+    String getName() {
+        return name;
+    }
+}
