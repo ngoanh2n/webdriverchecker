006f5dd0a9a8c7f0e6f21346a847c0d04e41a493
ngoanh2n <ngoanh2n@gmail.com>
2022-12-22 13:32:44 +0700

    [wdc] Add tests for :webdriverchecker-testng

---
+++ webdriverchecker-testng/src/test/java/com/github/ngoanh2n/wdc/BeforeClassTest.java
@@ -0,0 +1,31 @@
+package com.github.ngoanh2n.wdc;
+
+import com.github.ngoanh2n.RuntimeError;
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+/**
+ * @author Ho Huu Ngoan (ngoanh2n@gmail.com)
+ * @version 2.4.0
+ * @since 2022-12-22
+ */
+public class BeforeClassTest extends WDCTestNGTest {
+    @BeforeClass
+    void beforeClass() {
+        Assert.assertThrows(RuntimeError.class, WebDriverChecker::getWD);
+        driver = createWebDriver();
+        Assert.assertNotNull(WebDriverChecker.getWD());
+    }
+
+    @BeforeMethod
+    void beforeMethod() {
+        Assert.assertNotNull(WebDriverChecker.getWD());
+    }
+
+    @Test
+    void test() {
+        Assert.assertNotNull(WebDriverChecker.getWD());
+    }
+}
---
+++ webdriverchecker-testng/src/test/java/com/github/ngoanh2n/wdc/WDCTestNGTest.java
@@ -0,0 +1,33 @@
+package com.github.ngoanh2n.wdc;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+
+/**
+ * @author Ho Huu Ngoan (ngoanh2n@gmail.com)
+ * @version 2.4.0
+ * @since 2022-12-22
+ */
+public abstract class WDCTestNGTest {
+    static WebDriver driver;
+
+    @AfterMethod
+    void afterMethod() {
+        Assert.assertNotNull(WebDriverChecker.getWD());
+    }
+
+    @AfterClass
+    void afterClass() {
+        Assert.assertNotNull(WebDriverChecker.getWD());
+        driver.quit();
+    }
+
+    static WebDriver createWebDriver() {
+        WebDriverManager.chromedriver().setup();
+        return new ChromeDriver();
+    }
+}
