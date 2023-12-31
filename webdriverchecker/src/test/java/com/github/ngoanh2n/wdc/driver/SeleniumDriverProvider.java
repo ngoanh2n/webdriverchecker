835e6bfef8c580a90c340c97f050240583be7b31
ngoanh2n <ngoanh2n@gmail.com>
2023-12-31 22:42:17 +0700

    [wdc] Do not use WDM

--- webdriverchecker/src/test/java/com/github/ngoanh2n/wdc/driver/SeleniumDriverProvider.java
+++ webdriverchecker/src/test/java/com/github/ngoanh2n/wdc/driver/SeleniumDriverProvider.java
@@ -2,8 +2,12 @@ package com.github.ngoanh2n.wdc.driver;
 
 import com.github.ngoanh2n.Property;
 import com.github.ngoanh2n.RuntimeError;
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.safari.SafariDriver;
 
 /**
  * @author ngoanh2n
@@ -11,22 +15,13 @@ import org.openqa.selenium.WebDriver;
 public abstract class SeleniumDriverProvider {
     public static WebDriver createDriver() {
         String browser = Property.ofString("wdc.browser").getValue();
-
-        switch (browser) {
-            case "chrome":
-                return WebDriverManager.chromedriver().create();
-            case "safari":
-                return WebDriverManager.safaridriver().create();
-            case "firefox":
-                return WebDriverManager.firefoxdriver().create();
-            case "edge":
-                return WebDriverManager.edgedriver().create();
-            case "opera":
-                return WebDriverManager.operadriver().create();
-            case "ie":
-                return WebDriverManager.iedriver().create();
-            default:
-                throw new RuntimeError("Unknown browser: " + browser);
-        }
+        return switch (browser) {
+            case "chrome" -> new ChromeDriver();
+            case "safari" -> new SafariDriver();
+            case "firefox" -> new FirefoxDriver();
+            case "edge" -> new EdgeDriver();
+            case "ie" -> new InternetExplorerDriver();
+            default -> throw new RuntimeError("Unknown browser: " + browser);
+        };
     }
 }
